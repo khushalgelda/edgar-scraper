37bb9d0dd98492cd6715b6e2f263d50c915f0861
khushalgelda <khushalgelda@gmail.com>
2021-03-03 23:51:06 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,12 @@
-import json
+from ratemate import RateLimit
 import requests
 import datetime
 from bs4 import BeautifulSoup as soup
 from twilio.rest import Client
+import concurrent.futures
 
 base_url = 'https://www.sec.gov/Archives/edgar/daily-index/'
+rate_limit = RateLimit(max_count=10, per=1)
 
 
 class Entry:
@@ -27,10 +29,11 @@ def cik_for(ticker):
 
 # print(cik_for('ibm'))
 def doc_url(url):
+    rate_limit.wait()
     response = requests.get(url)
-    print(response.status_code)
+    # response = requests.get(url, proxies=get_proxy())
     if response.status_code != 200:
-        print('No url found.')
+        print(f'Filing link not available - {url}')
         return None
 
     decode_html = soup(response.text, 'html.parser')
@@ -41,27 +44,34 @@ def doc_url(url):
 
 def parse_master_idx(url):
     response = requests.get(url)
-    print(response.status_code)
     if response.status_code != 200:
         print('No record found for this day.')
         return None
-
+    links_to_request = []
     daily_dict = {}
+    i = 0
     for x in response.text.split('\n'):
         if '.txt' in x:
             pipe_split = x.split('|')
-            daily_dict[pipe_split[0]] = {}
-            daily_dict[pipe_split[0]]['form_type'] = pipe_split[2]
-            # daily_dict[pipe_split[0]]['date_filed'] = datetime.datetime.strptime(
-            #     pipe_split[3][0:4] + ' ' + pipe_split[3][4:6] + ' ' + pipe_split[3][6:8], '%Y %m %d')
-            daily_dict[pipe_split[0]]['date_filed'] = pipe_split[3][0:4] + '-' + pipe_split[3][4:6] + '-' + pipe_split[
-                                                                                                                3][6:8]
-            daily_dict[pipe_split[0]]['link'] = ['https://www.sec.gov/Archives/' + pipe_split[4].split('.')[
-                0] + '-index.html']
-            href = doc_url(daily_dict[pipe_split[0]]['link'][0])
-            daily_dict[pipe_split[0]]['link'].append(href)
+            daily_dict[i] = {}
+            daily_dict[i]['cik'] = pipe_split[0]
+            daily_dict[i]['form_type'] = pipe_split[2]
+            daily_dict[i]['date_filed'] = pipe_split[3][0:4] + '-' + pipe_split[3][4:6] + '-' + pipe_split[3][6:8]
+            html_link = 'https://www.sec.gov/Archives/' + pipe_split[4].split('.')[
+                0] + '-index.html'
+            links_to_request.append(html_link)
+            # if i == 5:
+            #     break
+            i = i + 1
+    print('Total Filings: {}'.format(i))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        html_extracted_links = executor.map(doc_url, list(links_to_request))
+    i = 0
+    for link in html_extracted_links:
+        daily_dict[i]['link'] = link
+        i = i + 1
+    # print(json.loads(json.dumps(daily_dict[pipe_split[0]])))
 
-            print(json.loads(json.dumps(daily_dict[pipe_split[0]])))
     return daily_dict
 
 
@@ -75,7 +85,7 @@ def crawl_url(baseurl, start_date, end_date):
             quarter = 'QTR2'
         elif date.month <= 9:
             quarter = 'QTR3'
-        elif date.month <= 12:
+        else:
             quarter = 'QTR4'
         url = baseurl + str(date.year) + '/' + quarter + '/' + 'master.' + date.strftime('%Y%m%d') + '.idx'
         print('Parsing data from {}'.format(url))
@@ -88,12 +98,12 @@ def main():
     crawl_url(base_url, datetime.datetime(2021, 2, 22),
               datetime.datetime(2021, 2, 22))
     account_sid = 'AC7cdfca43c5f8cb4a21201224954179c7'
-    auth_token = 'ffd5b80938868db4169769269d3c9405'
+    auth_token = '313ed88a692f37dfc434de5a136e1525'
     client = Client(account_sid, auth_token)
     client.messages.create(
         from_='whatsapp:+14155238886',
-        body='Run complete.',
-        to='whatsapp:+919314964063'
+        body=f'Edgar scraper run completed.',
+        to='whatsapp:+917411873829'
     )
 
 
