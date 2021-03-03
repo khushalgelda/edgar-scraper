bf37303db4332dd18d949eb672fd9297c34f24de
khushalgelda <khushalgelda@gmail.com>
2021-03-04 00:29:23 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,45 +1,44 @@
-from ratemate import RateLimit
-import requests
-import datetime
-from bs4 import BeautifulSoup as soup
-from twilio.rest import Client
 import concurrent.futures
+import datetime
+import requests
+from bs4 import BeautifulSoup as soup
+from ratemate import RateLimit
 
 base_url = 'https://www.sec.gov/Archives/edgar/daily-index/'
 rate_limit = RateLimit(max_count=10, per=1)
 
 
 class Entry:
-    def __init__(self, cik, form_type, date, link):
+    def __init__(self, cik=0, form_type='', date='', html_link='', doc_link=''):
         self.cik = cik
         self.form_type = form_type
         self.date = date
-        self.link = link
+        self.html_link = html_link
+        self.doc_link = doc_link
 
 
-def cik_for(ticker):
-    ticker_cik_map_link = 'https://www.sec.gov/include/ticker.txt'
-    response = requests.get(ticker_cik_map_link)
-    decoded_response = response.text
-    ticket_cik = {}
-    for x in decoded_response.split('\n'):
-        ticket_cik[x.split('\t')[0]] = x.split('\t')[1]
-    return ticket_cik[ticker]
+# def cik_for(ticker):
+#     ticker_cik_map_link = 'https://www.sec.gov/include/ticker.txt'
+#     response = requests.get(ticker_cik_map_link)
+#     decoded_response = response.text
+#     ticket_cik = {}
+#     for x in decoded_response.split('\n'):
+#         ticket_cik[x.split('\t')[0]] = x.split('\t')[1]
+#     return ticket_cik[ticker]
 
 
-# print(cik_for('ibm'))
-def doc_url(url):
+def add_doc_url(obj):
     rate_limit.wait()
-    response = requests.get(url)
+    response = requests.get(obj.html_link)
     # response = requests.get(url, proxies=get_proxy())
     if response.status_code != 200:
-        print(f'Filing link not available - {url}')
+        print(f'Filing link not available - {obj.html_link}')
         return None
 
     decode_html = soup(response.text, 'html.parser')
-    href = 'https://www.sec.gov/' + \
-           decode_html.find('table', {'class': 'tableFile'}).findAll('tr')[1].findAll('td')[2].a['href']
-    return href
+    obj.doc_link = 'https://www.sec.gov/' + \
+                   decode_html.find('table', {'class': 'tableFile'}).findAll('tr')[1].findAll('td')[2].a['href']
+    return obj
 
 
 def parse_master_idx(url):
@@ -47,47 +46,55 @@ def parse_master_idx(url):
     if response.status_code != 200:
         print('No record found for this day.')
         return None
-    links_to_request = []
     daily_dict = {}
+    daily_objects_pre = []
     i = 0
     for x in response.text.split('\n'):
         if '.txt' in x:
             pipe_split = x.split('|')
             daily_dict[i] = {}
-            daily_dict[i]['cik'] = pipe_split[0]
-            daily_dict[i]['form_type'] = pipe_split[2]
-            daily_dict[i]['date_filed'] = pipe_split[3][0:4] + '-' + pipe_split[3][4:6] + '-' + pipe_split[3][6:8]
-            html_link = 'https://www.sec.gov/Archives/' + pipe_split[4].split('.')[
+            obj = Entry()
+            obj.cik = int(pipe_split[0])
+            obj.form_type = pipe_split[2]
+            obj.date_filed = pipe_split[3][0:4] + '-' + pipe_split[3][4:6] + '-' + pipe_split[3][6:8]
+            obj.html_link = 'https://www.sec.gov/Archives/' + pipe_split[4].split('.')[
                 0] + '-index.html'
-            links_to_request.append(html_link)
+            daily_objects_pre.append(obj)
             # if i == 5:
             #     break
             i = i + 1
     print('Total Filings: {}'.format(i))
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        html_extracted_links = executor.map(doc_url, list(links_to_request))
-    i = 0
-    for link in html_extracted_links:
-        daily_dict[i]['link'] = link
-        i = i + 1
-    # print(json.loads(json.dumps(daily_dict[pipe_split[0]])))
+        daily_objects_post = executor.map(add_doc_url, daily_objects_pre)
 
-    return daily_dict
+    # print(json.loads(json.dumps(daily_dict[pipe_split[0]])))
+    # for x in daily_objects_post:
+    #     print(type(x.cik))
+    #     print(x.form_type)
+    #     print(x.date_filed)
+    #     print(x.html_link)
+    #     print(x.doc_link)
+    return daily_objects_post
+
+
+def create_url(baseurl, date):
+    if date.month <= 3:
+        quarter = 'QTR1'
+    elif date.month <= 6:
+        quarter = 'QTR2'
+    elif date.month <= 9:
+        quarter = 'QTR3'
+    else:
+        quarter = 'QTR4'
+    url = baseurl + str(date.year) + '/' + quarter + '/' + 'master.' + date.strftime('%Y%m%d') + '.idx'
+    return url
 
 
 def crawl_url(baseurl, start_date, end_date):
     date = start_date
     all_entries = []
     while date <= end_date:
-        if date.month <= 3:
-            quarter = 'QTR1'
-        elif date.month <= 6:
-            quarter = 'QTR2'
-        elif date.month <= 9:
-            quarter = 'QTR3'
-        else:
-            quarter = 'QTR4'
-        url = baseurl + str(date.year) + '/' + quarter + '/' + 'master.' + date.strftime('%Y%m%d') + '.idx'
+        url = create_url(baseurl, date)
         print('Parsing data from {}'.format(url))
         all_entries.append(parse_master_idx(url))
         date = date + datetime.timedelta(days=1)
@@ -97,14 +104,7 @@ def crawl_url(baseurl, start_date, end_date):
 def main():
     crawl_url(base_url, datetime.datetime(2021, 2, 22),
               datetime.datetime(2021, 2, 22))
-    account_sid = 'AC7cdfca43c5f8cb4a21201224954179c7'
-    auth_token = '313ed88a692f37dfc434de5a136e1525'
-    client = Client(account_sid, auth_token)
-    client.messages.create(
-        from_='whatsapp:+14155238886',
-        body=f'Edgar scraper run completed.',
-        to='whatsapp:+917411873829'
-    )
 
 
-main()
+if __name__ == '__main__':
+    main()
