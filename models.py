7dd341e8fc800009b79eab75f5ebdeb41fb4bc8e
Bhojal Gelda <bgelda@us.ibm.com>
2021-03-10 08:06:34 -0600

    introducing peewee ORM

---
+++ models.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import peewee
+from playhouse.pool import PooledMySQLDatabase
+
+#db = peewee.MySQLDatabase("mydb", host="localhost", port=3306, user="root", passwd="root")
+
+db = PooledMySQLDatabase(
+    'mydb',
+    max_connections=32,
+    stale_timeout=300,  # 5 minutes.
+    user='root',
+    passwd='root')
+
+
+class BaseModel(peewee.Model):
+    """A base model that will use our MySQL database"""
+    created = peewee.DateTimeField(default=datetime.now())
+
+    class Meta:
+        database = db
+
+
+class CIK(BaseModel):
+    cik = peewee.IntegerField(unique=True)
+    ticker = peewee.CharField(max_length=15, unique=True)
+    indexes = (
+        (('cik', 'ticker'), True),
+    )
+
+    def __enter__(self):
+        return self
+
+
+class EdgarEntry(BaseModel):
+    cik = peewee.ForeignKeyField(CIK, backref='entries')
+    form_type = peewee.CharField(max_length=10)
+    date = peewee.DateTimeField()
+    html_link = peewee.CharField()
+    doc_link = peewee.CharField()
+
+
+# simple utility function to create tables
+def create_tbls():
+    with db:
+        db.create_tables([CIK, EdgarEntry])
--- scraper.py
+++ scraper.py
@@ -1,11 +1,20 @@
 import concurrent.futures
 import datetime
+import os
+
 import requests
 from bs4 import BeautifulSoup as soup
+from peewee import chunked
 from ratemate import RateLimit
+from models import EdgarEntry, db, create_tbls, CIK
 
 base_url = 'https://www.sec.gov/Archives/edgar/daily-index/'
 rate_limit = RateLimit(max_count=10, per=1)
+# keeping CIK to ticker mapping in memory as well for fast access, this is supposed to be refreshed if the user
+# requested a ticker for which an entry in CIK table does not exist, we get CIK-Ticker mapping again from SEC and
+# refresh this map in load_cik_ticker
+ticker_to_cik = {}
+cik_to_ticker = {}
 
 
 class Entry:
@@ -17,17 +26,58 @@ class Entry:
         self.doc_link = doc_link
 
 
-# def cik_for(ticker):
-#     ticker_cik_map_link = 'https://www.sec.gov/include/ticker.txt'
-#     response = requests.get(ticker_cik_map_link)
-#     decoded_response = response.text
-#     ticket_cik = {}
-#     for x in decoded_response.split('\n'):
-#         ticket_cik[x.split('\t')[0]] = x.split('\t')[1]
-#     return ticket_cik[ticker]
+def load_cik_ticker():
+    print('Loading ticker and CIK mappings in the database...')
+    ticker_cik_map_link = 'https://www.sec.gov/include/ticker.txt'
+    response = requests.get(ticker_cik_map_link)
+    decoded_response = response.text
+    pks = []  # primary keys
+    rows = []
+    counter = 0
+    for x in decoded_response.split('\n'):
+        tkr = x.split('\t')[0]
+        cik = x.split('\t')[1]
+        ticker_to_cik[tkr] = [cik]
+        cik_to_ticker[cik] = [tkr]
+        rows.append(CIK(ticker=tkr, cik=cik))
+
+    print(len(rows), 'ticker to CIK mappings found on', ticker_cik_map_link)
+    for cik in cik_to_ticker:
+        pk = CIK.insert(ticker=cik_to_ticker[cik][0], cik=cik).on_conflict_replace().execute()
+        ticker_to_cik[cik_to_ticker[cik][0]].append(pk)
+        cik_to_ticker[cik].append(pk)
+
+        counter += 1
+        if counter % 500 == 0:
+            print('{} tiker and CIK loaded'.format(counter))
+    print('ticker and CIK mapping load COMPLETE')
+    # print('db is assigned to %r' % db)
+    # with db.atomic:
+    #     CIK.bulk_create(rows, batch_size=500)
 
 
-def add_doc_url(obj):
+def insert_new_cik(ticker):
+    ticker_cik_map_link = 'https://www.sec.gov/include/ticker.txt'
+    response = requests.get(ticker_cik_map_link)
+    decoded_response = response.text
+    for x in decoded_response.split('\n'):
+        tkr = x.split('\t')[0]
+        cik = x.split('\t')[1]
+        if ticker is not None and ticker == tkr:
+            CIK.insert(ticker=tkr, cik=cik).on_conflict_ignore().execute()
+            return cik
+
+    return None
+
+
+def cik_exists(ticker):
+    cik = CIK.select().where(ticker=ticker)
+    if cik is None or cik.cik is None:
+        return insert_new_cik(ticker)
+    return cik
+
+
+def add_doc_link_to_obj(obj):
     rate_limit.wait()
     response = requests.get(obj.html_link)
     # response = requests.get(url, proxies=get_proxy())
@@ -41,10 +91,10 @@ def add_doc_url(obj):
     return obj
 
 
-def parse_master_idx(url):
+def get_edgar_daily_objects(url):
     response = requests.get(url)
     if response.status_code != 200:
-        print('No record found for this day.')
+        print('No record found for this day (possibly a weekend).')
         return None
     daily_dict = {}
     daily_objects_pre = []
@@ -60,16 +110,38 @@ def parse_master_idx(url):
             obj.html_link = 'https://www.sec.gov/Archives/' + pipe_split[4].split('.')[
                 0] + '-index.html'
             daily_objects_pre.append(obj)
-            # if i == 5:
-            #     break
             i = i + 1
     print('Total Filings: {}'.format(i))
+    rows = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        # daily_objects_post = executor.map(add_doc_url, daily_objects_pre)
-        daily_objects_post = [executor.submit(add_doc_url, obj) for obj in daily_objects_pre]
-        # for x in concurrent.futures.as_completed(daily_objects_post):
-        #     print(x.result().cik)
-    # return daily_objects_post
+        # daily_objects_post = executor.map(add_doc_link_to_obj, daily_objects_pre)
+        daily_objects_post = {executor.submit(add_doc_link_to_obj, obj) for obj in daily_objects_pre}
+        # with db.atomic:
+        #     CIK.bulk_create(daily_objects_post, batch_size=500)
+        # # CIK.bulk_create(rows, batch_size=500)
+
+        for future in concurrent.futures.as_completed(daily_objects_post):
+            entry = future.result()
+            print(entry)
+            try:
+                rows.append({'cik': entry.cik, 'form_type': entry.form_type, 'date': entry.date,
+                             'html_link': entry.html_link, 'doc_link': entry.doc_link})
+            except KeyError:
+                print('The following CIK was not found anymore \n', entry.__str__())
+                continue
+            if rows is not None and len(rows) % 100 == 0:
+                print('writing {} rows to database'.format(len(rows)))
+                with db.atomic():
+                    EdgarEntry.insert_many(rows, fields=[EdgarEntry.cik, EdgarEntry.form_type, EdgarEntry.date,
+                                                          EdgarEntry.html_link, EdgarEntry.doc_link]).execute()
+                rows = []
+
+        if len(rows) < 100:
+            print('writing {} rows to database'.format(len(rows)))
+            with db.atomic():
+                EdgarEntry.insert_many(rows).execute()
+        print('done backilling for a day')
+    return daily_objects_post
 
 
 def create_url(baseurl, date):
@@ -91,15 +163,20 @@ def crawl_url(baseurl, start_date, end_date):
     while date <= end_date:
         url = create_url(baseurl, date)
         print('Parsing data from {}'.format(url))
-        all_entries.append(parse_master_idx(url))
+        all_entries.append(get_edgar_daily_objects(url))
         date = date + datetime.timedelta(days=1)
     return all_entries
 
 
+# when you're ready to start querying, remember to connect
 def main():
+    load_cik_ticker()
     crawl_url(base_url, datetime.datetime(2021, 1, 1),
               datetime.datetime(2021, 1, 31))
 
 
-if __name__ == '__main__':
-    main()
+db.connect()
+create_tbls()
+main()
+if not db.is_closed():
+    db.close()
